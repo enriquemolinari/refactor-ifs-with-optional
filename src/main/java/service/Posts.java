7bdd659ecbe4b124d2dc69d764294f1edecd25c9
Enrique <emolinari@jusrionegro.gov.ar>
2024-11-01 15:05:24 -0300

    extract method - read latest Post from cache, luego inline variable

--- src/main/java/service/Posts.java
+++ src/main/java/service/Posts.java
@@ -27,14 +27,16 @@ public class Posts {
         this.emf = emf;
     }
 
-    public List<LatestsPost> latestPosts(Long authorId) {
-        Optional<String> valueFromCache = Optional.empty();
-
+    private Optional<String> latestPostsFromCache(Long authorId) {
+        Optional<String> valueFromCache;
         try (Jedis jedis = new Jedis(REDIS_HOST, REDIS_PORT)) {
             valueFromCache = Optional.ofNullable(jedis.get(authorId.toString())); //Feo: devuelve null si no esta... me obliga la IF
         }
+        return valueFromCache;
+    }
 
-        return valueFromCache.map(jsonString -> {
+    public List<LatestsPost> latestPosts(Long authorId) {
+        return latestPostsFromCache(authorId).map(jsonString -> {
             Gson gson = new Gson();
             Type listType = new TypeToken<ArrayList<LatestsPost>>() {
             }.getType();
@@ -53,25 +55,5 @@ public class Posts {
                 return lastestsPosts;
             });
         });
-
-//        if (valueFromCache != null) {
-//            Gson gson = new Gson();
-//            Type listType = new TypeToken<ArrayList<LatestsPost>>() {
-//            }.getType();
-//            List<LatestsPost> latestsPosts = gson.fromJson(valueFromCache, listType);
-//            return latestsPosts;
-//        } else {
-//            return new Tx(emf).inTx(em -> {
-//                var query = em.createQuery("from Post order by fechaPublicacion desc", Post.class).setMaxResults(2);
-//                List<Post> resultList = query.getResultList();
-//                var lastestsPosts = resultList.stream().map(p -> p.toLatest()).toList();
-//                Gson gson = new GsonBuilder().registerTypeAdapter(LocalDateTime.class, new LocalDateTimeTypeAdapter()).create();
-//                String json = gson.toJson(lastestsPosts);
-//                try (Jedis jedis = new Jedis(REDIS_HOST, REDIS_PORT)) {
-//                    jedis.set(authorId.toString(), json);
-//                }
-//                return lastestsPosts;
-//            });
-//        }
     }
 }
